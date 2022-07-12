00c07444215f3198b6dc555d14ce920e8feef43d
sgoshaye <60760554+sgoshaye@users.noreply.github.com>
2022-07-12 13:24:29 -0400

    Update final_assignment.sql

--- final_assignment.sql
+++ final_assignment.sql
@@ -4,8 +4,8 @@ Write a query that finds students who do not take CS180.
 
 SELECT * FROM students
 WHERE student_no NOT IN (SELECT student_no
-						 FROM student_enrollment
-						 WHERE course_no = 'CS180');
+			 FROM student_enrollment
+			 WHERE course_no = 'CS180');
 
 /* Problem 2. 
 Write a query to find students who take CS110 or CS107 but not both.
@@ -16,10 +16,10 @@ FROM students s JOIN student_enrollment se
 ON s.student_no = se.student_no
 AND se.course_no IN ('CS110', 'CS107')
 AND s.student_no NOT IN (SELECT a.student_no
-						 FROM student_enrollment a JOIN student_enrollment b
-						 ON a.student_no = b.student_no
-						 AND a.course_no = 'CS110'
-						 AND b.course_no = 'CS107');
+			 FROM student_enrollment a JOIN student_enrollment b
+			 ON a.student_no = b.student_no
+			 AND a.course_no = 'CS110'
+			 AND b.course_no = 'CS107');
 
 /*
 Problem 3. 
@@ -30,8 +30,8 @@ SELECT s.*
 FROM students s JOIN student_enrollment se
 ON s.student_no = se.student_no
 AND s.student_no NOT IN (SELECT student_no
-						 FROM student_enrollment
-						 WHERE course_no != 'CS220');
+			 FROM student_enrollment
+			 WHERE course_no != 'CS220');
 
 /*
 Problem 4. 
@@ -54,5 +54,5 @@ Write a query to find students who are older than at most two other students.
 SELECT s1.*
 FROM students s1
 WHERE 2 >= (SELECT count(*)
-			FROM students s2
-			WHERE s2.age < s1.age);
+	    FROM students s2
+	    WHERE s2.age < s1.age);
